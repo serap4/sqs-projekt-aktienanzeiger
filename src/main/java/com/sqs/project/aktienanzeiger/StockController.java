dd9f3b06df748ba58b029d034ca3d0baec3b3a79
serap kaya <serap.kaya@stud.th-rosenheim.de>
2024-06-18 00:34:04 +0200

    Fixing maintainability issues

--- src/main/java/com/sqs/project/aktienanzeiger/StockController.java
+++ src/main/java/com/sqs/project/aktienanzeiger/StockController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class StockController {
@@ -15,12 +17,12 @@ public class StockController {
     }
 
     @GetMapping("/stock/{symbol}/{date}")
-    public StockData getStockData(@PathVariable String symbol,@PathVariable String date) throws Exception {
+    public StockData getStockData(@PathVariable String symbol,@PathVariable String date) throws IOException {
         return stockService.getStockData(symbol, date);
     }
 
     @GetMapping("/stock/delete/{symbol}/{date}")
-    public String deleteStockData(@PathVariable String symbol,@PathVariable String date) {
+    public String deleteStockData(@PathVariable String symbol,@PathVariable String date){
         return stockService.deleteStockData(symbol, date);
     }
 
