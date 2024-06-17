cb1361304eb4538bc0393079a412163724f480b9
serap kaya <serap.kaya@stud.th-rosenheim.de>
2024-06-18 00:39:54 +0200

    Fixing maintainability issues

--- src/main/java/com/sqs/project/aktienanzeiger/StockService.java
+++ src/main/java/com/sqs/project/aktienanzeiger/StockService.java
@@ -2,7 +2,6 @@ package com.sqs.project.aktienanzeiger;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
