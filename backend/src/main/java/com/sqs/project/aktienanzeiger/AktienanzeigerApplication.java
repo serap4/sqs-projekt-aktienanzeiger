af02f446c5e226cd38d705fe04758af9c05cf5ed
serap kaya <serap.kaya@stud.th-rosenheim.de>
2024-06-21 10:40:43 +0200

    Folder created for backend

---
+++ backend/src/main/java/com/sqs/project/aktienanzeiger/AktienanzeigerApplication.java
@@ -0,0 +1,12 @@
+package com.sqs.project.aktienanzeiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class AktienanzeigerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AktienanzeigerApplication.class, args);
+	}
+
+}
