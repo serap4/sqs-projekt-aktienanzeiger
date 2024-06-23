8fa70bd169a0ce8eccc3093d93b620f802cb6cd2
serap kaya <serap.kaya@stud.th-rosenheim.de>
2024-06-23 04:34:16 +0200

    created ArchitectureTest

--- backend/src/test/java/com/sqs/project/aktienanzeiger/ArchitectureTest.java
+++ backend/src/test/java/com/sqs/project/aktienanzeiger/ArchitectureTest.java
@@ -1,19 +1,44 @@
 package com.sqs.project.aktienanzeiger;
 
-import com.tngtech.archunit.junit.ArchTest;
-import com.tngtech.archunit.lang.ArchRule;
-
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import org.junit.jupiter.api.Test;
 
 public class ArchitectureTest {
 
-    @ArchTest
-    public static final ArchRule services_should_not_have_cyclic_dependencies =
-            classes().that().resideInAPackage("..service..")
-                    .should().haveNoCycles();
+    @Test
+    void services_should_not_depend_on_controllers() {
+        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.sqs.project.aktienanzeiger");
 
-    @ArchTest
-    public static final ArchRule controllers_should_only_depend_on_services =
-            classes().that().resideInAPackage("..controller..")
-                    .should().onlyDependOnClassesThat().resideInAnyPackage("..service..", "..controller..");
+        ArchRuleDefinition.noClasses()
+                .that().resideInAnyPackage("..service..")
+                .should().dependOnClassesThat().resideInAnyPackage("..controller..")
+                .check(importedClasses);
+    }
+
+    @Test
+    void controllers_should_only_depend_on_services() {
+        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.sqs.project.aktienanzeiger");
+
+        ArchRuleDefinition.classes()
+                .that().resideInAnyPackage("..controller..")
+                .should().onlyDependOnClassesThat().resideInAnyPackage("..service..")
+                .orShould().dependOnClassesThat().resideInAnyPackage("..model..")
+                .check(importedClasses);
+    }
+
+    @Test
+    void services_should_only_depend_on_other_services_or_models() {
+        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.sqs.project.aktienanzeiger");
+
+        ArchRuleDefinition.classes()
+                .that().resideInAnyPackage("..service..")
+                .should().onlyDependOnClassesThat().resideInAnyPackage("..service..")
+                .orShould().dependOnClassesThat().resideInAnyPackage("..model..")
+                .orShould().dependOnClassesThat().resideInAnyPackage("java..")
+                .orShould().dependOnClassesThat().resideInAnyPackage("javax..")
+                .orShould().dependOnClassesThat().resideInAnyPackage("org.springframework..")
+                .check(importedClasses);
+    }
 }
