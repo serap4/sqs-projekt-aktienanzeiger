f39334788752d4e6eda0283f949f3d11ecd84554
serap kaya <serap.kaya@stud.th-rosenheim.de>
2024-06-23 04:27:26 +0200

    created ArchitectureTest

--- backend/src/test/java/com/sqs/project/aktienanzeiger/ArchitectureTest.java
+++ backend/src/test/java/com/sqs/project/aktienanzeiger/ArchitectureTest.java
@@ -4,14 +4,13 @@ import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 public class ArchitectureTest {
 
     @ArchTest
-    public static final ArchRule services_should_not_depend_on_each_other =
-            noClasses().that().resideInAPackage("..service..")
-                    .should().dependOnEachOther();
+    public static final ArchRule services_should_not_have_cyclic_dependencies =
+            classes().that().resideInAPackage("..service..")
+                    .should().haveNoCycles();
 
     @ArchTest
     public static final ArchRule controllers_should_only_depend_on_services =
